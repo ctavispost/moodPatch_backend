33ea29c81a7846bcb59140d5699df429947443ab
ctavispost <ctavispost@gmail.com>
2020-12-15 21:18:03 -0800

    Add query for all colors so they don't have to be grabbed individually

--- controllers/color.js
+++ controllers/color.js
@@ -1,17 +1,30 @@
 const db = require('../models');
 
+const index = (req, res) => {
+    db.color.findAll()
+        .then((colors) => {
+            if (!colors) return res.json({
+                message: 'No colors found in db.'
+            });
+            res.status(200).json({ colors: colors });
+        })
+        .catch(error => {
+            res.status(500).send(error)
+        });
+}
+
 const show = (req, res) => {
     db.color.findByPk(req.params.id)
         .then((foundColor) => {
             if (!foundColor) return res.json({
-                message: 'Post with provided ID not found.'
+                message: 'Color with provided ID not found.'
             })
-
             res.status(200).json({ color: foundColor });
         })
         .catch(error => res.status(500).send(error));
 }
 
 module.exports = {
+    index,
     show
 }
